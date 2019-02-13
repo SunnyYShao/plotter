08e5df583a52fc04058dede24ede55d2aa304e68
Sono <sonoshah@gmail.com>
2019-02-13 10:53:47 -0800

    uploaded test scripts, with modification, and a single script to test

---
+++ scripts/testing_script.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(tidycensus)
+source("scripts/aapidata_style.R")
+source("scripts/finalize_plot.R")
+
+test_dta <- get_acs(geography = "state",variables = "B02018_001",year = 2017)
+
+test_plot <- test_dta %>% 
+  top_n(10,wt = estimate) %>% 
+  select(NAME, estimate, moe) %>% 
+  ggplot(aes(x = reorder(NAME,estimate), y = estimate))+
+  geom_bar(stat="identity")+
+  # geom_pointrange(aes(ymin=estimate-moe, ymax = estimate + moe))+
+  coord_flip()+
+  scale_y_continuous(labels=scales::comma)+
+  geom_text(aes(label= scales::comma(estimate)))+
+  aapidata_style()+
+  labs(title = "Test Title")
+  
+test_plot
+
+finalise_plot(plot_name = test_plot,
+              source_name = "test",width_pixels = 1400, height_pixels = 800,
+              logo_image_path = "~/Dropbox/Shared/AAPIData HQ/Projects/opinion database/aapidata-apia-logo-v2.png",save_filepath = "dta/test.png")
