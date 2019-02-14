074e143c06c90ee1ae19491468967299b7fd35b4
SunnyYShao <yshao006@ucr.edu>
2019-02-14 13:27:39 -0800

    color scheme

---
+++ color_scheme.R
@@ -0,0 +1,112 @@
+# Defining color schemes 
+
+# AAPI DATA ---------------------------------------------------------------
+
+#all possible colors for infographics
+aapidata <- c(
+  `orange`       = "#EE8B46",
+  `light orange` = "#EFA875",
+  `tan`       = "#EDDD9D",
+  `light blue`     = "#A3C8D1",
+  `blue`     = "#63BBD1",
+  `grey` = "#C6C4C4",
+  `light red`  = "#E28A86",
+  `red`  = "#DC7070")
+
+#create a function that call the color by name
+aapidata_cols <- function(...) {
+  cols <- c(...)
+  
+  if (is.null(cols))
+    return (aapidata)
+  
+  aapidata[cols]
+}
+aapidata_cols()
+
+#create different list of color-schemes
+#each color palette need to have the same number of groups as the table we are trying to graph
+aapidata_palettes <- list(
+  #the code pull colors from both end (instead of  left-to-right)
+  
+  `scale5` = aapidata_cols("red", "light red", "grey", "light orange", "orange"),
+  `scale4` = aapidata_cols("red", "light red", "light orange", "orange"),
+  `scale3` = aapidata_cols("red", "grey", "orange"),
+  `scale2
+  ` = aapidata_cols("red", "grey", "orange"),
+  `cat4`  = aapidata_cols("light orange", "orange", "light blue",  "blue"),
+  
+  `cat2`  = aapidata_cols("orange", "blue"),
+  
+  `partisan_DRIOD`   = aapidata_cols("grey", "tan", "light orange", "orange", "red", "blue"),
+  
+  `partisan_RDSD` = aapidata_cols("grey", "orange", "blue", "red"),
+  
+  `partisan_DRD` = aapidata_cols("grey", "red", "blue"),
+  
+  `partisan3_SD`  = aapidata_cols( "grey", "light blue", "blue"),
+  
+  `partisan3_SR`  = aapidata_cols("light blue", "grey", "red"),
+  
+  `partisan_DROD` = aapidata_cols("grey", "orange", "red", "blue"),
+  
+  `scale3_1m`  = aapidata_cols("blue", "tan", "orange"),
+  
+  `scale4_party_2m`  = aapidata_cols("blue", "grey", "tan", "red"),
+  
+  `scale4_party_DNDR`  = aapidata_cols("red", "grey", "tan", "blue"),
+  
+  `scale5_1m`  = aapidata_cols("blue", "light blue", "tan", "light orange", "orange"),
+  
+  `scale5`  = aapidata_cols("grey", "blue", "light blue", "light orange", "orange"),
+  
+  # `scale6_2m`  = aapidata_cols("blue", "light blue", "grey", "tan", "light orange", "orange"),
+  
+  `scale4_2m`  = aapidata_cols("blue", "grey", "tan", "orange"),
+  
+  `special6`   = aapidata_cols("grey", "tan", "light orange", "orange", "red", "blue"),
+  
+  `ordered`    = aapidata_cols("grey", "tan", "light blue", "blue", "light orange", "orange"),
+  
+  `ordered5`    = aapidata_cols("grey", "light blue", "blue", "light orange", "orange"),
+  
+  `ordered4`    = aapidata_cols("grey", "tan", "light orange", "orange"),
+  
+  `yes_no_d`   = aapidata_cols("grey", "blue", "orange"),
+  
+  `yes_no`   = aapidata_cols("blue", "orange"),
+  
+  `yn_BD`   = aapidata_cols("grey", "blue", "tan", "orange"))
+
+#create a function to 
+aapidata_pal <- function(palette = "categorical", reverse = FALSE, ...) {
+  pal <- aapidata_palettes[[palette]]
+  
+  if (reverse) pal <- rev(pal)
+  
+  colorRampPalette(pal, ...)
+}
+#test for example, if I want 
+aapidata_pal(palette = "yes_no", reverse = FALSE)
+
+
+
+scale_fill_aapidata <- function(palette = "scale", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- aapidata_pal(palette = palette, reverse = reverse)
+  if (discrete) {
+    discrete_scale("fill", paste0("aapidata_", palette), palette = pal, ...)
+  } else {
+    scale_fill_gradientn(colours = pal(256), ...)
+  }
+}
+
+
+scale_color_aapidata <- function(palette = "scale", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- aapidata_pal(palette = palette, reverse = reverse)
+  if (discrete) {
+    discrete_scale("fill", paste0("aapidata_", palette), palette = pal, ...)
+  } else {
+    scale_color_gradientn(colours = pal(256), ...)
+  }
+}
+
---
+++ color_test.R
@@ -0,0 +1,68 @@
+library(tidyverse)
+library(readxl)
+source("color_scheme.R")
+dta <- read_csv("/Users/sunnyshao/Desktop/color_test.csv")
+  plot <- dta %>%
+    ggplot(aes(x = group, y = prop, fill = key)) +
+    geom_col(width = .7) +
+    scale_y_continuous(labels = scales::percent, limits = c(0, 1.1)) +
+    hrbrthemes::theme_modern_rc(grid = "", plot_title_size = 32, axis_text_size = 25, caption_size = 12, subtitle_size = 24) +
+    labs(
+      y = "AAPI Share of Electorate", x = "", fill = "",
+      caption = "Source: 2018 Asian American Voter Survey (AAVS) by APIAVote and AAPI Data"
+    ) +
+    theme(
+      axis.ticks.length = unit(-1, "cm"),
+      axis.ticks = element_blank(),
+      legend.position = "top"
+    ) +
+    theme(
+      axis.title.x = element_blank(),
+      plot.title = element_text(hjust = 0.5),
+      plot.subtitle = element_text(hjust = 0.5),
+      axis.text.x = element_blank(),
+      axis.ticks.x = element_blank(),
+      axis.text.y = element_text(colour = "#dfd8c0", face = "plain"),
+      plot.background = element_rect(fill = "#3F3F3F", color = "#3F3F3F"),
+      panel.background = element_rect(
+        fill = "#3F3F3F",
+        colour = "#3F3F3F")) +
+    coord_flip() +
+    geom_text(family = "Roboto Condensed", fontface = "bold", size = 7, position = position_stack(vjust = 0.5), aes(label = ifelse(prop < .03, NA, scales::percent(prop, 2)))) +
+    scale_color_aapidata(palette = "scale", guide = guide_legend(reverse = T)) + # this make sure the legend order match with stacked bar order
+    guides(fill = guide_legend(reverse = TRUE)) +
+    theme(
+      legend.position = "top",
+      legend.justification = 0.4,
+      legend.text = element_text(size = 18),
+      plot.caption = element_text(hjust = .75, vjust = -5))
+  
+  plot
+  # add \n after main title this decrease the spacing between logo and title
+  
+  # Generating Regular Graphic ----------------------------------------------
+  plot_reg <- gridExtra::grid.arrange(
+    top = grobTree(
+      rectGrob(gp = gpar(fill = "#3F3F3F", col = "#3F3F3F")),
+      textGrob(paste(title, "\n", sep = ""), gp = gpar(col = "white", fontface = "bold", family = "Roboto Condensed", fontsize = 32)),
+      textGrob(paste("\n\n", question, sep = ""), gp = gpar(cex = .8, col = "white", fontface = "italic", family = "Roboto Condensed", fontsize = 24))
+    ),
+    plot, padding = unit(3, "cm")
+  )
+  ggsave(plot_reg,
+         filename = paste("infographics_ethnicities/", file, ".png", sep = ""),
+         width = 14, height = 7, units = "in", dpi = "retina"
+  )
+  plot2 <- image_read(paste("infographics_ethnicities/", file, ".png", sep = ""))
+  logo_raw <- image_read("~/Dropbox/AAPIData HQ/Projects/opinion database/aapidata-apia-logo-v2.png") # load image in the right directory
+  logo <- logo_raw %>%
+    image_scale("800x300") %>% # ORIGINAL
+    # image_scale("1000x300") %>%
+    image_background("#303030", flatten = TRUE) %>%
+    image_border("#303030", "1000x40")
+  # final_plot <- image_append(image_scale(c(logo,plot2 ), "1400x800"), stack = TRUE)
+  final_plot <- image_append(image_scale(c(logo, plot2), "1400x800"), stack = TRUE)
+  final_plot <- image_resize(final_plot, "1024x76
+                             8")
+  image_write(final_plot, path = paste("infographics_ethnicities/", file, ".png", sep = ""))
+}
