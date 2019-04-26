df3740827004c196de003e38a3220865cb0b483a
Sunny Shao <yshao006@ucr.edu>
2019-04-26 16:45:56 -0700

    update stacked bar graph plotter function

--- scripts/plotter_stacked_bar.R
+++ scripts/plotter_stacked_bar.R
@@ -1,9 +1,9 @@
 # plotter stacked bar -----------------------------------------------------
 
 #percent 
-plotter_stacked_pct <- function(dta, file_name, color_type, label_size, subtitle, caption){
-  title <- dta[1,4]
-  plot <- dta %>%
+plotter_stacked_pct <- function(data, color_type, style_function, logo_image_path, label_color, label_size, subtitle, footnote, directory, file_name){
+  title <- data[1,4]
+  plot <- data %>%
     ggplot(aes(x = reorder(group, key_order), y = estimate, fill = key))+
     geom_bar(stat="identity",
              width=.7,
@@ -12,24 +12,24 @@ plotter_stacked_pct <- function(dta, file_name, color_type, label_size, subtitle
     scale_y_continuous(labels=scales::percent, limits = c(0, 1.1))+
     geom_text(aes(label = ifelse(estimate < 0.03, NA, scales::percent(estimate, 1))),
               position = position_stack(reverse = TRUE,vjust = .5),
-              size=label_size, color = "white")+
-    aapidata_style()+
+              size=label_size, color = label_color)+
+    style_function()+
     scale_fill_aapidata(palette = color_type)+
     theme(legend.position = "top")+
     theme(legend.position = 'top',
           legend.spacing.x = unit(1.0, 'cm'))+
-    labs(title = title,subtitle = subtitle, fill="",x="", y="", caption=caption)
+    labs(title = title,subtitle = subtitle, fill="",x="", y="", caption=footnote)
   
   finalise_plot(plot_name = plot,
                 width_pixels = 1400, height_pixels = 800,
-                logo_image_path = "/Users/sunnyshao/Dropbox/AAPIData HQ/2019 APALA Report/logov3.png", ## YOU WILL NEED TO
-                save_filepath = paste0("final_graph/", file_name))
+                logo_image_path = logo_image_path,
+                save_filepath = paste0(directory, file_name))
 }
 
 #percent vertical
-plotter_Vstacked_pct <- function(dta, file_name, color_type, label_size, subtitle, caption){
-  title <- dta[1,4]
-  plot <- dta %>%
+plotter_Vstacked_pct <- function(data, color_type, style_function, logo_image_path, label_color, label_size, subtitle, footnote, directory, file_name){
+  title <- data[1,4]
+  plot <- data %>%
     ggplot(aes(x = reorder(group, key_order), y = estimate, fill = key))+
     geom_bar(stat="identity",
              width=.7,
@@ -37,16 +37,16 @@ plotter_Vstacked_pct <- function(dta, file_name, color_type, label_size, subtitl
     scale_y_continuous(labels=scales::percent)+
     geom_text(aes(label = ifelse(estimate < .008, NA, scales::percent(estimate, 0.1))),
               position = position_stack(vjust = .5),
-              size=label_size, color = "#3f3f3f")+
-    aapidata_Vstyle()+
+              size=label_size, color = label_color)+
+    style_function()+
     scale_fill_aapidata(palette = color_type)+
     theme(legend.position = "top")+
     theme(legend.position = 'top',
           legend.spacing.x = unit(1.0, 'cm'))+
-    labs(title = title,subtitle = subtitle, fill="",x="", y="", caption=caption)
+    labs(title = title,subtitle = subtitle, fill="",x="", y="", caption=footnote)
   
   finalise_plot(plot_name = plot,
                 width_pixels = 1400, height_pixels = 800,
-                logo_image_path = "/Users/sunnyshao/Dropbox/AAPIData HQ/2019 APALA Report/logov3.png", ## YOU WILL NEED TO
-                save_filepath = paste0("final_graph/", file_name))
+                logo_image_path = logo_image_path,
+                save_filepath = paste0(directory, file_name))
 }
