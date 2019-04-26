9757380fb8e4c4b3673d7ee5df26cee888e3ce3c
Sunny Shao <yshao006@ucr.edu>
2019-04-26 16:46:58 -0700

    update the example plotting script

--- examples4plotter_functions.R
+++ examples4plotter_functions.R
@@ -8,13 +8,11 @@ source("color_scheme.R")
 source("scripts/aapidata_style.R")
 source("scripts/finalize_plot.R")
 source("scripts/plotter_single_bar.R")
-
-
+source("scripts/plotter_stacked_bar.R")
+source("scripts/plotter_multi_single_bar.R")
 # cleaning the data -----------------------------------------------------------------
 ## 1. load the data
-dta <- read_excel("examples/example.xlsx")
-
-##2. make sure the final data has four columns in the order of group, estimate, key, title
+dta <- read_excel("examples/data_single_bar.xlsx")
 
 #clean data for single bar graph count
 single_bar_count <- dta %>% 
@@ -38,11 +36,19 @@ single_bar_pct_unlim <- dta %>%
   filter(!group %in% c("Asian Am AVERAGE", "NHPI AVERAGE","US AVERAGE")) %>% 
   mutate(key = "growth", title = "2010-2017 Population Growth by Detailed AAPI Groups")
 
-# plot --------------------------------------------------------------------
+#clean data for stacked bar graph percent
+stacked_bar_pct <- read_csv("examples/data_stacked_bar.csv")
+stacked_bar_pct$key <- factor(stacked_bar_pct$key, levels=c("Extremely important", "Very important", "Somewhat important", "Not that important", "Don't Know"))
 
-#how to use the function
+#clean data for vertical stacked bar graph percent
+Vstacked_bar_pct <- read_csv("examples/data_Vstacked.csv")
 
-#############################################
+#clean data for multiple single bar graph percent
+multi_bar_pct <- read_csv("examples/data_multi_bar.csv") %>% 
+  mutate(key_order = case_when(
+    key=="Men" ~1,
+    key=="Women" ~2))
+# documentation - functions ############################################
 #data --> the dataframe you want to plot
 #color_type --> please refer to "color_scheme.R"
 #style_function: options include "aapidata_style", "aapidata_Vstyle" (vertical bar), "aapidata_style_smallx" (smaller y-axis text)
@@ -53,8 +59,9 @@ single_bar_pct_unlim <- dta %>%
 #footnote: source/reference
 #directory: directory of where you want to store the final graph
 #file_name: file name of the final graph
-#############################################
 
+
+# single bar graphs - demonstration --------------------------------------------------------------------
 
 #single bar for count data
 plotter_single_count(data = single_bar_count, color_type = "single_dorange", 
@@ -88,3 +95,35 @@ plotter_single_pct_unlim_small(data = single_bar_pct_unlim, color_type = "single
                          subtitle = " ", 
                          footnote = "Source: American Community Survey 2017 1-Year files\n",
                          directory = "examples/", file_name = "single_bar_pct_unlim_smallx_example.png")
+
+# multiple single bar graphs
+plotter_multi_pct(data = multi_bar_pct, color_type = "yes_no", 
+                  style_function = aapidata_style, logo_image_path = "examples/logo_aapidata.png",
+                  label_color = "#ffffff", label_size = 7, 
+                  subtitle = " ",
+                  footnote = "Source: American Community Survey 2017\n",
+                  directory = "examples/", file_name = "multi_single_bar_example.png")
+
+
+
+# stacked bar graph - demonstration ---------------------------------------
+
+#horizontal stacked bar
+plotter_stacked_pct(data = stacked_bar_pct, color_type = "scale5_2", 
+                    style_function = aapidata_style_smallx(), logo_image_path = "examples/logo_aapidata.png",
+                    label_color = "#ffffff", label_size = 7, 
+                    subtitle = "How important is it that workers can retire with dignity and financial security?\n", 
+                    footnote = "Source: Asian American Voter Survey 2018 \n",
+                    directory = "examples/", file_name = "stacked_bar_pct_smallX_example.png")
+
+#vertial stacked bar
+plotter_Vstacked_pct(data = Vstacked_bar_pct, color_type = "yes_no", 
+                    style_function = aapidata_Vstyle, logo_image_path = "examples/logo_aapidata.png",
+                    label_color = "#ffffff", label_size = 7, 
+                    subtitle = " ",
+                    footnote = "Source: CPS Ongoing Rotation Group 2010-2018 \n",
+                    directory = "examples/", file_name = "Vstacked_bar_pct_smallX_example.png")
+
+
+
+
